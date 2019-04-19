3f4f6b1e4fa68e15b985507f665c8722420e13e9
GIS <GerasimenkoIS@it.mos.ru>
2019-04-19 17:34:37 +0300

    add two more simple rest tests

--- src/test/java/my/api/rest/FirstTest.java
+++ src/test/java/my/api/rest/FirstTest.java
@@ -12,8 +12,8 @@ public class FirstTest {
     public void doRegisterTest() {
         JSONObject jsonObject = new JSONObject();
         jsonObject
-                .append("email", "milli@mail.ru")
-                .append("name", "Машенька")
+                .append("email", "1234milli@mail.ru")
+                .append("name", "1223Машенька")
                 .append("password", "1");
 
         given()
@@ -21,5 +21,38 @@ public class FirstTest {
                 .contentType(ContentType.JSON)
                 .body(jsonObject.toString())
                 .post(" http://users.bugred.ru/tasks/rest/doregister");
+    }
+
+    @Test
+    public void doRegisterLogResponseTest() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject
+                .put("email", "Hello.Rest.The.World@earth.com")
+                .put("name", "Hello Rester")
+                .put("password", "1");
+
+        System.out.println(jsonObject.toString());
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(jsonObject.toString())
+                .post(" http://users.bugred.ru/tasks/rest/doregister")
+                .print();
+    }
+
+    @Test
+    public void doRegisterTooSimpleTest() {
+
+
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"email\": \"milli3@mail.ru\",\n" +
+                        "    \"name\": \"Машенька3\",\n" +
+                        "    \"password\": \"1\"\n" +
+                        "}")
+                .post(" http://users.bugred.ru/tasks/rest/doregister")
+                .print();
     }
 }
