a1bd85b1b91ad9ace7b6970ca0761d80dcdf7fbe
GIS <GerasimenkoIS@it.mos.ru>
2019-05-08 14:55:35 +0300

    User.java: add new fields & update toString method

--- src/test/java/my/api/rest/dataobjects/User.java
+++ src/test/java/my/api/rest/dataobjects/User.java
@@ -10,7 +10,18 @@ public class User {
     public String fathername1;
 
     public String hobby;
+    public String cat;
+    public String dog;
+    public String parrot;
+    public String cavy;
     public String hamster;
+    public String squirrel;
+    public String phone;
+    public String adres;
+    public String gender;
+    public String birthday;
+    public String date_start;
+    public String inn;
 
     public User setName(String name) {
         this.name = name;
@@ -44,8 +55,52 @@ public class User {
         this.hobby = hobby;
         return this;
     }
+    public User setCat(String cat) {
+        this.cat = cat;
+        return this;
+    }
+    public User setDog(String dog) {
+        this.dog = dog;
+        return this;
+    }
+    public User setParrot(String parrot) {
+        this.parrot = parrot;
+        return this;
+    }
+    public User setCavy(String cavy) {
+        this.cavy = cavy;
+        return this;
+    }
     public User setHamster(String hamster) {
         this.hamster = hamster;
+        return this;
+    }
+    public User setSquirrel(String squirrel) {
+        this.squirrel = squirrel;
+        return this;
+    }
+    public User setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+    public User setAdres(String adres) {
+        this.adres = adres;
+        return this;
+    }
+    public User setGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+    public User setBirthday(String birthday) {
+        this.birthday = birthday;
+        return this;
+    }
+    public User setDate_start(String date_start) {
+        this.date_start = date_start;
+        return this;
+    }
+    public User setInn(String inn) {
+        this.inn = inn;
         return this;
     }
 
@@ -66,8 +121,41 @@ public class User {
             if (hobby != null) {
                 objInfo += "hobby: '" + hobby + "', ";
             }
+            if (cat != null) {
+                objInfo += "cat: '" + cat + "', ";
+            }
+            if (dog != null) {
+                objInfo += "dog: '" + dog + "', ";
+            }
+            if (parrot != null) {
+                objInfo += "parrot: '" + parrot + "', ";
+            }
+            if (cavy != null) {
+                objInfo += "cavy: '" + cavy + "', ";
+            }
             if (hamster != null) {
                 objInfo += "hamster: '" + hamster + "', ";
+            }
+            if (squirrel != null) {
+                objInfo += "squirrel: '" + squirrel + "', ";
+            }
+            if (phone != null) {
+                objInfo += "phone: '" + phone + "', ";
+            }
+            if (adres != null) {
+                objInfo += "adres: '" + adres + "', ";
+            }
+            if (gender != null) {
+                objInfo += "gender: '" + gender + "', ";
+            }
+            if (birthday != null) {
+                objInfo += "birthday: '" + birthday + "', ";
+            }
+            if (date_start != null) {
+                objInfo += "date_start: '" + date_start + "', ";
+            }
+            if (inn != null) {
+                objInfo += "inn: '" + inn + "', ";
             }
 
             if (objInfo.length() > 2) {
