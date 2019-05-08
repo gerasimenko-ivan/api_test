90bf3f6745ce2afa2207ad038dbd883ad252b91a
GIS <GerasimenkoIS@it.mos.ru>
2019-05-08 13:19:12 +0300

    User.java: add new fields (surname1, fathername1, hobby) & update toString method

--- src/test/java/my/api/rest/dataobjects/User.java
+++ src/test/java/my/api/rest/dataobjects/User.java
@@ -5,32 +5,45 @@ public class User {
     public String email;
     public String password;
 
+    public String surname1;
     public String name1;
+    public String fathername1;
 
+    public String hobby;
     public String hamster;
 
     public User setName(String name) {
         this.name = name;
         return this;
     }
-
     public User setEmail(String email) {
         this.email = email;
         return this;
     }
-
     public User setPassword(String password) {
         this.password = password;
         return this;
     }
 
 
+    public User setSurname1(String surname1) {
+        this.surname1 = surname1;
+        return this;
+    }
     public User setName1(String name1) {
         this.name1 = name1;
         return this;
     }
+    public User setFathername1(String fathername1) {
+        this.fathername1 = fathername1;
+        return this;
+    }
 
 
+    public User setHobby(String hobby) {
+        this.hobby = hobby;
+        return this;
+    }
     public User setHamster(String hamster) {
         this.hamster = hamster;
         return this;
@@ -40,10 +53,19 @@ public class User {
     public String toString() {
         if (name == null && email == null && password == null) {
             String objInfo = "{";
+            if (surname1 != null) {
+                objInfo += "surname1: '" + surname1 + "', ";
+            }
             if (name1 != null) {
                 objInfo += "name1: '" + name1 + "', ";
             }
+            if (fathername1 != null) {
+                objInfo += "fathername1: '" + fathername1 + "', ";
+            }
 
+            if (hobby != null) {
+                objInfo += "hobby: '" + hobby + "', ";
+            }
             if (hamster != null) {
                 objInfo += "hamster: '" + hamster + "', ";
             }
