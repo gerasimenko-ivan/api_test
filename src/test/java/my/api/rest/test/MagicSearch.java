d4b0ce574b22e98101ccedb87696f10625e3f225
GIS <GerasimenkoIS@it.mos.ru>
2019-05-17 11:00:27 +0300

    MagicSearch.java：full set of checks is now completed

--- src/test/java/my/api/rest/test/MagicSearch.java
+++ src/test/java/my/api/rest/test/MagicSearch.java
@@ -2,6 +2,7 @@ package my.api.rest.test;
 
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
+import my.api.rest.dataobjects.Company;
 import my.api.rest.dataobjects.User;
 import org.json.JSONObject;
 import org.testng.annotations.BeforeClass;
@@ -22,7 +23,7 @@ public class MagicSearch {
     public void createTestUsers() {
         // we need:
         // 1 user with 1 company (& 1 user with 2+ companies) - 231 SC
-        // 2/+/ users (without company(ies)) names: testStatusCodeUser12345678901234567890END1, testStatusCodeUser12345678901234567890END2 - 231, 232 SC
+        // 2/+/ users (without company(ies)) names: testStatusCodeUser_twoUsers_noCompanies_12345678901234567890END1, testStatusCodeUser_twoUsers_noCompanies_12345678901234567890END2 - 231, 232 SC
         // 2/+/ companies (without user(s)) names: testStatusCodeCompany12345678901234567890END1, testStatusCodeCompany12345678901234567890END2 - 234 SC
         //
 
@@ -30,9 +31,22 @@ public class MagicSearch {
             createUser(testUser);
     }
 
+    Random rnd = new Random();
     private User userNoCompanies = new User().setName("hello12345678901234567890END").setEmail("hello12345678901234567890END@mail.ml").setPassword("1");
+    private String twoUsersNoCompanies_samePartOfName = "testStatusCodeUser_twoUsers_noCompanies_12345678901234567890END";
+    private User userCompanyOwner = new User().setName("companyowner12345678901234567890end").setEmail("companyowner12345678901234567890end@mail.ml").setPassword("1");
+    private String theSameNamePartOfOneUserAndOneCompany = "1userand1company" + rnd.nextInt(1000000000) + rnd.nextInt(1000000000) + rnd.nextInt(1000000000) + "end";
+    private String theSameNamePartOfOneUserAndTwoCompanies = "1userand2comp_" + rnd.nextInt(1000000000) + rnd.nextInt(1000000) + rnd.nextInt(1000000) + "end";
+    private String theSameNamePartOfTwoUsersAndOneCompany = "2usersand1comp_" + rnd.nextInt(1000000000) + rnd.nextInt(1000000) + rnd.nextInt(1000000) + "end";
     private User[] testUsers = new User[] {
-            userNoCompanies
+            userNoCompanies,
+            new User().setName(twoUsersNoCompanies_samePartOfName + "1").setEmail(twoUsersNoCompanies_samePartOfName + "1@mail.ml").setPassword("1"),
+            new User().setName(twoUsersNoCompanies_samePartOfName + "2").setEmail(twoUsersNoCompanies_samePartOfName + "2@mail.ml").setPassword("1"),
+            userCompanyOwner,
+            new User().setName(theSameNamePartOfOneUserAndOneCompany + "_user").setEmail(theSameNamePartOfOneUserAndOneCompany + "_user@mail.ml").setPassword("1"),
+            new User().setName(theSameNamePartOfOneUserAndTwoCompanies + "_user").setEmail(theSameNamePartOfOneUserAndTwoCompanies + "_user@mail.ml").setPassword("1"),
+            new User().setName(theSameNamePartOfTwoUsersAndOneCompany + "_user1").setEmail(theSameNamePartOfTwoUsersAndOneCompany + "_user1@mail.ml").setPassword("1"),
+            new User().setName(theSameNamePartOfTwoUsersAndOneCompany + "_user2").setEmail(theSameNamePartOfTwoUsersAndOneCompany + "_user2@mail.ml").setPassword("1")
     };
 
     private void createUser(User testUser) {
@@ -67,8 +81,50 @@ public class MagicSearch {
         }
     }
 
+    private String testCompanyName = "testCompany" + rnd.nextInt(1000000000) + rnd.nextInt(1000000000) + rnd.nextInt(1000000000) + "END";
+    private String twoCompanies_samePartOfName = "twoCompaniesStatusCodeTest" + rnd.nextInt(1000000000) + rnd.nextInt(1000000000) + rnd.nextInt(1000000000) + "END";
+
+    private Company[] testCompanies = new Company[] {
+            new Company()
+                    .setCompany_name(testCompanyName)
+                    .setCompany_type("ООО")
+                    .setCompany_users(new String[]{userCompanyOwner.email})
+                    .setEmail_owner(userCompanyOwner.email),
+            new Company()
+                    .setCompany_name(twoCompanies_samePartOfName + "1")
+                    .setCompany_type("ООО")
+                    .setCompany_users(new String[]{userCompanyOwner.email})
+                    .setEmail_owner(userCompanyOwner.email),
+            new Company()
+                    .setCompany_name(twoCompanies_samePartOfName + "2")
+                    .setCompany_type("ООО")
+                    .setCompany_users(new String[]{userCompanyOwner.email})
+                    .setEmail_owner(userCompanyOwner.email),
+            new Company()
+                    .setCompany_name(theSameNamePartOfOneUserAndOneCompany + "_company")
+                    .setCompany_type("ООО")
+                    .setCompany_users(new String[]{userCompanyOwner.email})
+                    .setEmail_owner(userCompanyOwner.email),
+            new Company()
+                    .setCompany_name(theSameNamePartOfOneUserAndTwoCompanies + "_company1")
+                    .setCompany_type("ООО")
+                    .setCompany_users(new String[]{userCompanyOwner.email})
+                    .setEmail_owner(userCompanyOwner.email),
+            new Company()
+                    .setCompany_name(theSameNamePartOfOneUserAndTwoCompanies + "_company2")
+                    .setCompany_type("ООО")
+                    .setCompany_users(new String[]{userCompanyOwner.email})
+                    .setEmail_owner(userCompanyOwner.email),
+            new Company()
+                    .setCompany_name(theSameNamePartOfTwoUsersAndOneCompany + "_company")
+                    .setCompany_type("ООО")
+                    .setCompany_users(new String[]{userCompanyOwner.email})
+                    .setEmail_owner(userCompanyOwner.email)
+    };
+
     @BeforeClass
-    public void createTestCompanies() {
+    public void createTestZCompanies() {
+        System.out.println("----------------\ncreateTestCompanies");
         // we need:
         // 1 user with 1 company (& 1 user with 2+ companies) - 231 SC
         // 2/+/ users (without company(ies)) names: testStatusCodeUser12345678901234567890END1, testStatusCodeUser12345678901234567890END2 - 231, 232 SC
@@ -76,6 +132,58 @@ public class MagicSearch {
         //
 
 
+
+        for (Company testCompany: testCompanies)
+            createCompany(testCompany);
+    }
+
+    private void createCompany(Company testCompany) {
+        JSONObject testCompanyAsJson = new JSONObject();
+        String company_nameKey = "company_name";
+        String company_typeKey = "company_type";
+        String company_usersKey = "company_users";
+        String email_ownerKey = "email_owner";
+        testCompanyAsJson
+                .put(company_nameKey, testCompany.company_name)
+                .put(company_typeKey, testCompany.company_type)
+                .put(company_usersKey, testCompany.company_users)
+                .put(email_ownerKey, testCompany.email_owner);
+        System.out.println("Request: \n" + testCompanyAsJson.toString() + "\n");
+
+        Response response = given()
+                .when()                         //.accept(ContentType.XML) - response content type
+                .contentType(ContentType.JSON)  // request content type
+                .body(testCompanyAsJson.toString())
+                .post("http://users.bugred.ru/tasks/rest/createcompany");
+
+        assertThat("Check response code", response.statusCode(), equalTo(200));
+
+        System.out.println("Response:\n" + response.getBody().asString());
+        JSONObject jsonResponse = new JSONObject(response.getBody().asString());
+
+        if (jsonResponse.has("type") && jsonResponse.has("id_company") && jsonResponse.has("company")) {
+            assertThat(jsonResponse.get("type").toString(), equalTo("success"));
+        } else {
+            fail("Unexpected response body contents (expect created user info OR error message that user exists)");
+        }
+
+        /* SUCCESS EXAMPLE
+        {
+            "type": "success",
+            "id_company": 29,
+            "company": {
+                "name": "erftghj",
+                "type": "ООО",
+                "inn": "",
+                "ogrn": "",
+                "kpp": "",
+                "phone": "",
+                "adress": "",
+                "users": [
+                    "testtest@mail.ml"
+                ]
+            }
+        }*/
     }
 
     ////// SUCCESSFUL SEARCH
@@ -93,16 +201,78 @@ public class MagicSearch {
     @Test
     public void checkStatusCode231_oneUserFound() {
         // 1 user no comp
+        JSONObject singleUser =
+                new JSONObject()
+                        .put("query", userNoCompanies.name);
+
+        magicSearchPostRequestWithStatusCodeCheck(singleUser, 231);
         // 1 user - 1 comp in this user ?
         // ? 1 user - 2 comp in this user
-
     }
 
     @Test
     public void checkStatusCode232_twoUserPlusAndNoCompaniesFound() {
         // 2 users no comp
+        JSONObject twoUsers =
+                new JSONObject()
+                        .put("query", twoUsersNoCompanies_samePartOfName);
+
+        magicSearchPostRequestWithStatusCodeCheck(twoUsers, 232);
     }
 
+    @Test
+    public void checkStatusCode233_oneCompanyAndNoUsersFound() {
+        JSONObject oneCompany =
+                new JSONObject()
+                        .put("query", testCompanyName);
+
+        magicSearchPostRequestWithStatusCodeCheck(oneCompany, 233);
+    }
+
+    @Test
+    public void checkStatusCode234_twoCompaniesAndNoUsersFound() {
+        JSONObject twoCompanies =
+                new JSONObject()
+                        .put("query", twoCompanies_samePartOfName);
+
+        magicSearchPostRequestWithStatusCodeCheck(twoCompanies, 234);
+    }
+
+    @Test
+    public void checkStatusCode235_oneCompanyAndOneUserFound() {
+        JSONObject oneUserOneCompany =
+                new JSONObject()
+                        .put("query", theSameNamePartOfOneUserAndOneCompany);
+
+        magicSearchPostRequestWithStatusCodeCheck(oneUserOneCompany, 235);
+    }
+
+    @Test
+    public void checkStatusCode235_twoCompaniesAndOneUserFound() {
+        JSONObject oneUserOneCompany =
+                new JSONObject()
+                        .put("query", theSameNamePartOfOneUserAndTwoCompanies);
+
+        magicSearchPostRequestWithStatusCodeCheck(oneUserOneCompany, 235);
+    }
+
+    @Test
+    public void checkStatusCode235_oneCompanyAndTwoUsersFound() {
+        JSONObject oneUserOneCompany =
+                new JSONObject()
+                        .put("query", theSameNamePartOfTwoUsersAndOneCompany);
+
+        magicSearchPostRequestWithStatusCodeCheck(oneUserOneCompany, 235);
+    }
+
+    @Test
+    public void checkStatusCode235_twoPlusCompaniesAndTwoPlusUsersFound() {
+        JSONObject oneUserOneCompany =
+                new JSONObject()
+                        .put("query", "end");
+
+        magicSearchPostRequestWithStatusCodeCheck(oneUserOneCompany, 235);
+    }
 
     //// CLIENT ERRORS
 
@@ -150,20 +320,21 @@ public class MagicSearch {
     private Object[] invalidPartyTypes(){
         return new String[] {
                 "all",
-                "ALOL",
-                "AL",
-                "AALL",
-                "ALLL",
+                "All",
+                "ALL ",
+                " ALL",
+
                 "user",
-                "USLER",
-                "SER",
-                "USERa",
+                "USER ",
                 " USER",
+                "USERS",
+
                 "company",
-                "COMPANYY",
                 "COMPANY ",
-                "CCOMPANY",
+                " COMPANY",
                 "COMPANIES",
+
+                "",
                 "null"
         };
     }
@@ -181,25 +352,29 @@ public class MagicSearch {
     private Object[] invalidTaskStatuses(){
         return new String[] {
                 "all",
-                "ALOL",
-                "AL",
-                "AALL",
-                "ALLL",
+                "All",
+                "ALL ",
+                " ALL",
+
                 "actual",
-                "aCTUAL",
-                "AACTUAL",
-                "ACTUALY",
+                "Actual",
                 " ACTUAL",
                 "ACTUAL ",
+                "ACTUALLY",
+
                 "complete",
+                "Complete",
                 "COMPLETE ",
                 " COMPLETE",
-                "cOMPLETE",
                 "COMPLETED",
+
                 "fail",
+                "Fail",
                 "FAILED",
                 "FAIL ",
                 " FAIL",
+
+                "",
                 "null",
 
                 "EVERY",
@@ -216,7 +391,7 @@ public class MagicSearch {
         JSONObject userWithInvalidInclude =
                 new JSONObject()
                         .put("query", userNoCompanies.name)
-                        .put("taskStatus", invalidInclude);
+                        .put("include", invalidInclude);
 
         magicSearchPostRequestWithStatusCodeCheck(userWithInvalidInclude, 459);
     }
@@ -224,52 +399,57 @@ public class MagicSearch {
     private Object[] invalidIncludes() {
         return new String[]{
                 "all",
-                "ALOL",
-                "AL",
-                "AALL",
-                "ALLL",
+                "All",
+                " ALL",
+                "ALL ",
+
                 "user",
-                "USLER",
-                "SER",
-                "USERa",
+                "User",
+                "USER ",
                 " USER",
+                "USERS",
+                "Users",
+
                 "company",
-                "COMPANYY",
+                "Company",
+                " COMPANY",
                 "COMPANY ",
-                "CCOMPANY",
                 "COMPANIES",
+
                 "task",
+                "Task",
                 "TASK ",
                 " TASK",
                 "TASKS",
-                "tASK",
                 "why",
                 "WHY ",
                 " WHY",
-                "",
-                "null",
 
-                "JOB",
-                "JOBS",
+                "",
+                "null"
         };
     }
+
+    // error 400 don't test
 
 
     ////////////////////////////// SUPPORT FUNCTION
 
-    private void magicSearchPostRequestWithStatusCodeCheck(JSONObject userDoesNotExist, int expectedStatusCode) {
-        System.out.println("--------------------------\nTest\nRequest: \n" + userDoesNotExist.toString() + "\nResponse:");
+    private void magicSearchPostRequestWithStatusCodeCheck(JSONObject requestQuery, int expectedStatusCode) {
+        System.out.println("--------------------------\nTest\nRequest: \n" + requestQuery.toString() + "\nResponse:");
 
         given()
                 .when()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
-                .body(userDoesNotExist.toString())
+                .body(requestQuery.toString())
                 .post("http://users.bugred.ru/tasks/rest/magicsearch")
                 .then()
                 .log()
                 .body()
                 .assertThat()
-                .statusCode(expectedStatusCode);
+                .statusCode(expectedStatusCode)
+                .log()
+                .status();
     }
 }
